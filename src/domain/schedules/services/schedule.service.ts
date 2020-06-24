ec424e91726217e22af191c30428e954bb76e140
Mai The Vinh <vinh.mai@hearti.io>
2020-06-25 00:19:10 +0700

    saa

--- src/domain/schedules/services/schedule.service.ts
+++ src/domain/schedules/services/schedule.service.ts
@@ -126,6 +126,7 @@ export class ScheduleService implements IScheduleService {
     async getSchedulesByDoctor(doctorId: string, day?: DayOfWeek): Promise<any> {
         const datas = await this.scheduleRepository.getScheduleByDoctor(doctorId, day);
 
+        
         const result = {
             MONDAY: [],
             TUESDAY: [],
